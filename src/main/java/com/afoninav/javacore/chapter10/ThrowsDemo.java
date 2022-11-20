1702b292b35ae53de588f48fa25250b44f829db2
aleksej <aleksej.afonin@gmail.com>
2022-11-20 19:00:53 +0700

    Пример исправлен. Добавлен throws, и try/catch

--- src/main/java/com/afoninav/javacore/chapter10/ThrowsDemo.java
+++ src/main/java/com/afoninav/javacore/chapter10/ThrowsDemo.java
@@ -4,17 +4,23 @@ package com.afoninav.javacore.chapter10;
  * Пример программы генерирующей исключение
  * без указания об этом.
  * Ошибка компиляции.
+ *
+ * Добавлен оператор throws в объявлении метода бросающего исключение
+ * и блок try/catch в методе main для обработки этого исключения.
+ *
  */
 
 public class ThrowsDemo {
-    static void throwOne() {
+    static void throwOne() throws IllegalAccessException {
         System.out.println("Внутри тела метода throwOne().");
-//        throw new IllegalAccessException("Demonstration");
+        throw new IllegalAccessException("Demonstration");
     }
 
     public static void main(String[] args) {
-        throwOne();
+        try {
+            throwOne();
+        } catch (IllegalAccessException e) {
+            System.out.println("Перехвачено исключение: " + e);
+        }
     }
-
-
 }
