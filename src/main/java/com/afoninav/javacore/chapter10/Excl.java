5d07fbe4f191c4afc51800f5af0f6f964a1f362f
aleksej <aleksej.afonin@gmail.com>
2022-11-20 14:45:05 +0700

    Исключительная ситуация при делении на ноль

---
+++ src/main/java/com/afoninav/javacore/chapter10/Excl.java
@@ -0,0 +1,12 @@
+package com.afoninav.javacore.chapter10;
+
+public class Excl {
+    static void subroutine () {
+        int d = 0;
+        int a = 10 / d;
+    }
+
+    public static void main (String[] args) {
+        Excl.subroutine();
+    }
+}
