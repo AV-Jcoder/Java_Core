d271f909bcec938cb52f183c6d9f8471aabce9b9
aleksej <aleksej.afonin@gmail.com>
2022-11-20 16:57:07 +0700

    Пример с исправленным порядком операторов catch

--- src/main/java/com/afoninav/javacore/chapter10/SuperSubCatch.java
+++ src/main/java/com/afoninav/javacore/chapter10/SuperSubCatch.java
@@ -1,14 +1,17 @@
 package com.afoninav.javacore.chapter10;
 
+/**
+ * Правильный порядок операторов catch
+ */
 public class SuperSubCatch {
     public static void main(String[] args) {
         try {
             int a = 0;
             int b = 42 / a;
-        } catch (Exception e) {
-            System.out.println("Перехват исключения общего класса Exception.");
         } catch (ArithmeticException e) {
             System.out.println("Этот код недостижим и считается ошибкой.");
+        } catch (Exception e) {
+            System.out.println("Перехват исключения общего класса Exception.");
         }
     }
 }
